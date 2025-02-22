9d94e7d70e878b6d0fceb709967440688133da56
PedroWatermann <pedro.programmer.pt@gmail.com>
2025-02-22 16:49:33 -0300

    Aula 21/02/25

---
+++ AppCep/AppCep/MainPage.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using AppCep.Service.Model;
+using AppCep.Service;
+
+namespace AppCep
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            btnBuscar.Clicked += buscarCep;
+        }
+
+        private void buscarCep(object sender, EventArgs e)
+        {
+            string cep = entCep.Text.Trim();
+            if (isValidCep(cep))
+            {
+                try
+                {
+                    ModEndereco endereco = SerViaCep.buscarEnderecoViaCep(cep);
+
+                    if (endereco != null)
+                    {
+                        lblResultado.Text = string.Format("Endereço: {2}, {3}, {0} {1}.", endereco.Localidade, endereco.Uf, endereco.Logradouro, endereco.Bairro);
+                        entCep.Text = "";
+                    }
+                    else
+                    {
+                        DisplayAlert("Erro", $"O CEP {cep} não foi encontrado!", "OK");
+                        entCep.Text = "";
+                    }
+                }
+                catch (Exception er)
+                {
+                    DisplayAlert("Erro Fatal", $"Erro: {er.Message}", "OK");
+                }
+            }
+        }
+
+        private bool isValidCep(string cep)
+        {
+            bool valido = true;
+
+            if (cep.Length != 8)
+            {
+                DisplayAlert("Erro", "CEP inválido!\n\nO CEP deve conter 8 números", "OK");
+                valido = false;
+            }
+
+            int novoCep = 0;
+            if (!int.TryParse(cep, out novoCep))
+            {
+                DisplayAlert("Erro", "O CEP deve conter apenas números!", "OK");
+                valido = false;
+            }
+
+            return valido;
+        }
+    }
+}
---
+++ AppCep/AppCep/Service/SerViaCep.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+using AppCep.Service.Model;
+using Newtonsoft.Json;
+
+namespace AppCep.Service
+{
+    public class SerViaCep
+    {
+        private static string enderecoUrl = "https://viacep.com.br/ws/{0}/json";
+
+        public static ModEndereco buscarEnderecoViaCep(string cep)
+        {
+            string novoEnderecoUrl = string.Format(enderecoUrl, cep);
+
+            WebClient wc = new WebClient(); // Abre um cliente para web: como se fosse um túnel para enviar requisições http na web
+            string conteudo = wc.DownloadString(novoEnderecoUrl); // Baixar e armazenar todas as strings do conteúdo do site
+            ModEndereco endereco = JsonConvert.DeserializeObject<ModEndereco>(conteudo); // Converte o json recebido 
+
+            return endereco.Cep == null ? null : endereco;
+        }
+    }
+}
